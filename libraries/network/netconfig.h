34d036adf4d60c9741e7520fd8d1becdf077280d
N00B <n00b@localhost.localdomain>
2021-04-04 03:51:42 +0200

    version 3.1.0

--- libraries/network/netconfig.h
+++ libraries/network/netconfig.h
@@ -12,6 +12,8 @@
 
 const uint8_t AES_INIT_MK[N_BLOCK]  PROGMEM = { 0x65 ,0x6e ,0x63 ,0x72 ,0x74 ,0x79 ,0x70 ,0x74 ,0x2d ,0x2d ,0x6d ,0x61 ,0x73 ,0x74 ,0x65 ,0x72 };
 
+const char DEFAULT_SSID[]           PROGMEM = "Your-WiFi-SSID";
+const char DEFAULT_PSK[]            PROGMEM = "Your-WiFi-passphrase";
 
 #endif /* INDOORINO_NETWORK */
 #endif /* SOURCE_NETCONFIG_H_ */
